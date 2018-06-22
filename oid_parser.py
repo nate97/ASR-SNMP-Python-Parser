2218bbd582fdd07ce0af73a0fad4ec82e8f08e3f
root <root@D106GS1.shawnee.lan>
2018-06-22 13:18:34 -0500

    Start working on OID manager/parser

--- oid_parser.py
+++ oid_parser.py
@@ -13,22 +13,23 @@ OID_STATSIN = 'ipIfStatsInOctets'
 OID_STATSOUT = 'ipIfStatsOutOctets'
 
 
-
 class OIDParser():
 
     def __init__(self):
-        print ('test')
+        pass
         
 
-
+    # KEEP OID LISTS IN HERE, [NOT] GLOBAL
     def OIDManager(self):
         # We will process and keep track of all OID data HERE
 
         # EXAMPLE #
         descrOIDS = self.getOIDs(OID_DESCR)
-        statsINOIDS = self.getOIDs(OID_STATSIN)
-        statsOUTOIDS = self.getOIDs(OID_STATSOUT)
+        statsINOIDS = self.getOIDs(OID_STATSIN) # Disable temp
+        #statsOUTOIDS = self.getOIDs(OID_STATSOUT)
 
+        self.parseOIDs(descrOIDS)
+        self.parseOIDs(statsINOIDS)
 
 
     def getOIDs(self, OIDType):    # Retrive a specific type of OID(s) ( Called to retrive list of OIDS from ASR, returns string from ASR )
@@ -38,23 +39,35 @@ class OIDParser():
         # RawOIDOutput contains all information retrived from the SNMPwalk bash command
         RawOIDOutput = self.externalProcess(combinedCommand)
 
-        print (RawOIDOutput)
-
         return RawOIDOutput
-        
 
 
-    def parseOIDs(self, OIDS):    # Parse ALL of the different types of oid lists ( Called AFTER getOIDs, parses string )
-        pass
+    def parseOIDs(self, oids):    # Parse ALL of the different types of oid lists ( Called AFTER getOIDs, parses string )
+
+        # Remove newlines and special characters, appends each line to a list
+        oids_list = oids.split("\\n")
+
+        for newline in oids_list:    # Iterate through the raw oid list
+
+            if " = " not in newline:    # If the string ( = ) WITH SPACES, is not in newline, SKIP
+                continue
+
+            stream_3 = newline.split(".")  # Split newline into 3 parts, OID description, port_channel, VLAN
+
+            try:
+                vlanTag = stream_3[2]  # Extract VLAN tag []
+                print(vlanTag)
+            except:
+                print ("Error: " + str(stream_3))
 
 
-
-    def parseDescrOIDS(self):
+    def parseDescrOIDS(self):    # Parses description OID type
         pass
         return OIDLIST
 
 
-
-    def parseOctetOIDS(self, type):
+    # Argument should specify wether octet is IN or OUT
+    def parseOctetOIDS(self, type):    # Parses IN or OUT octet OID type
         pass
+        # We will put a time stamp with the octets
         return OIDLIST
