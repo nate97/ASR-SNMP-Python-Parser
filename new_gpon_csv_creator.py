79c008d5fe160389e593d176d14c5f2116158fb0
root <root@D106GS1.shawnee.lan>
2018-08-17 16:35:30 -0500

    Close to merging gpon CMS

--- new_gpon_csv_creator.py
+++ new_gpon_csv_creator.py
@@ -15,21 +15,21 @@ class GponCombiner():
         self.first()
         self.second()
 
+        self.repeats()
+
 
 
     def first(self):
         self.externalProcess("awk 'NR>2' combine_gpon/gpon_ont_ge.csv > combine_gpon/gpon_ont_ge_nohead.csv")
         self.externalProcess('sort ' + 'combine_gpon/gpon_ont_ge_nohead.csv' +  ' > ' + 'combine_gpon/gpon_ont_ge_sorted.csv') # Sort
-        gponGeList = self.iterateGeCSV('combine_gpon/gpon_ont_ge_sorted.csv')
-
-        self.duplicates(gponGeList)
+        self.gponGeList = self.iterateGeCSV('combine_gpon/gpon_ont_ge_sorted.csv')
 
 
 
     def second(self):
         self.externalProcess("awk 'NR>2' combine_gpon/gpon_data.csv > combine_gpon/gpon_data_nohead.csv")
         self.externalProcess('sort ' + 'combine_gpon/gpon_data_nohead.csv' +  ' > ' + 'combine_gpon/gpon_data_sorted.csv') # Sort
-        gponDataList = self.iterateDataCSV('combine_gpon/gpon_data_sorted.csv')
+        self.gponDataList = self.iterateDataCSV('combine_gpon/gpon_data_sorted.csv')
 
 
 
@@ -37,9 +37,9 @@ class GponCombiner():
         lineList = []
         fileA = open(filename, 'r')
 
-        for line in fileA.readlines():
-            print (line)
-            lineList.append(line)
+        #for line in fileA.readlines():
+            #print (line)
+            #lineList.append(line)
 
         return lineList
 
@@ -89,9 +89,19 @@ class GponCombiner():
                 if id == " ":
                     continue
 
-                regionAndID = [region + ":" + id] # Create new individual column list
-                row = regionAndID + row # Append new data to our row list
-                gponDataList.append(row)
+
+                id = id.split("-GE")
+                id = id[0]
+                try:
+                    id2 = id.split("-", 1)
+                    test = (id2[1])
+
+                    regionAndID = [region + ":" + test] # Create new individual column list
+                    row = regionAndID + row # Append new data to our row list
+                    gponDataList.append(row)
+
+                except:
+                    continue
 
         csvAE.close()
 
@@ -99,20 +109,79 @@ class GponCombiner():
 
 
 
-    def duplicates(self, lists):
+    def repeats(self):
 
-        possibleDupes = []
-        realDuplicates = []
+        self.findMatch()
 
-        for data in lists:
-            possibleDupes.append(data[0])
+        new1 = self.remove_duplicates(self.gponGeList)
+
+        gponData2 = self.remove_duplicates(self.gponDataList)
+
+        #for x in new1:
+            #print (x[0])
 
 
-        noDupes = list(OrderedDict.fromkeys(possibleDupes).keys())
 
-        for x in lists:
-            if x[0] in lists:
-                print (x[0])
+    def remove_duplicates(self, listy):
+        newlist = [ii for n, ii in enumerate(listy) if ii not in listy[0][:n]]
+        return newlist
+
+
+
+    def findMatch(self):
+
+        newCSVLines = []
+
+        for d in self.gponGeList:
+
+            for a in self.gponDataList:
+
+                if d[0] in a[0]:
+
+                    print (d)
+                    print (d[0])
+                    print (d[1])
+                    print (d[2])
+                    print (d[3])
+                    print (d[4])
+                    print (d[5])
+                    print (d[6])
+                    print (d[7])
+                    print ("|||||||||||||||||||||||||||||||||")
+
+                    print (a[3])
+                    print (a[4])
+                    print (a[5])
+                    print (a[6])
+                    print (a[7])
+
+                    print (a)
+
+                    newCSVLines.append(d[0])
+                    newCSVLines.append(d[1])
+                    newCSVLines.append(d[2])
+                    newCSVLines.append(d[3])
+                    newCSVLines.append(d[4])
+                    newCSVLines.append(d[5])
+                    newCSVLines.append(d[6])
+                    newCSVLines.append(d[7])
+
+                    newCSVLines.append(a[3])
+                    newCSVLines.append(a[4])
+                    newCSVLines.append(a[5])
+                    newCSVLines.append(a[6])
+                    newCSVLines.append(a[7])
+
+        # WORKS!!!!! DON'T DELETE THIS COMM                    newCSVLines.append(a[])ENT!!!!!!!!!! USED AS REFERENCE!!!!!!!!!!!
+        """
+        for x in range(0, len(self.gponGeList)):
+            try:
+                if "Cave In Rock:1-1-94" in self.gponDataList[x]:
+                    print ("In lists")
+            except:
+                pass
+                
+        """
 
 
 
